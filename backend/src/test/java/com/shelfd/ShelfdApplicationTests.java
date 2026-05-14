8d7527f6cdbf85e08b731ce6884fd919e53e3410
LuxaJoy <nepomucenojulliana@gmail.com>
2026-05-14 18:28:49 +0800

    Builds and directories update

---
+++ backend/src/test/java/com/shelfd/ShelfdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shelfd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShelfdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
